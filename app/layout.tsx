dc0f1755846b482a3fbd5b66b7ac53df1953a0d0
zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2026-06-24 16:49:02 +0700

    feat(analytics): lazyload

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,9 +116,9 @@ export default function RootLayout({
       <body>{children}</body>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
